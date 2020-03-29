099aa525a3095f03e0dc278edb84d3f218fc01dd
Liadrinz <435438602@qq.com>
2020-03-29 17:16:50 +0800

    很sb的bug

--- start.py
+++ start.py
@@ -3,7 +3,7 @@ from config import config, update
 
 config['me'] = input('请输入QQ: ')
 update(config)
-if len(os.sys.argv) and os.sys.argv[1] == 'sh':
+if len(os.sys.argv) > 1 and os.sys.argv[1] == 'sh':
     os.system('.\start.sh')
 else:
     os.system('.\start.bat')
